a02b1dd0fce889fd4b7fbfc7fab6ffc5ec48738e
苏文龙 <543187000@qq.com>
2019-04-27 13:22:08 +0800

    1.0.4

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ func main() {
 
 	var bmRouter BmConfig.BmRouterConfig
 	bmRouter.GenerateConfig("BM_HOME")
-	//bmRouter.Port = "20199"
 	addr := bmRouter.Host + ":" + bmRouter.Port
 	fmt.Println("Listening on ", addr)
 	api := api2go.NewAPIWithResolver(version, &BmApiResolver.RequestURL{Addr: addr})
