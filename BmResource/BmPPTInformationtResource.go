048eeb70dac4e4e38d0371a5b1ab2a63ab76069c
苏文龙 <543187000@qq.com>
2019-05-15 10:46:41 +0800

    先改了一个测试一下

--- BmResource/BmPPTInformationtResource.go
+++ BmResource/BmPPTInformationtResource.go
@@ -3,6 +3,7 @@ package BmResource
 import (
 	//"bytes"
 	"errors"
+	"github.com/elodina/go-avro"
 	"github.com/PharbersDeveloper/CHC-PPT/BmDataStorage"
 	"github.com/PharbersDeveloper/CHC-PPT/BmModel"
 	"github.com/manyminds/api2go"
@@ -26,7 +27,10 @@ type PptinformationResource struct {
 	ChcpptStorage         *BmDataStorage.ChcpptStorage
 	ChcppttemplateStorage         *BmDataStorage.ChcppttemplateStorage
 }
-
+const (
+	schemaRepositoryUrl = "59.110.31.50:8081"
+	rawMetricsSchema    = `{"namespace": "net.elodina.kafka.metrics","type": "record","name": "PPT","fields": [{"data": "id"},{"include": "timings"}]}`
+)
 func (c PptinformationResource) NewPptinformationResource(args []BmDataStorage.BmStorage) PptinformationResource {
 	var cs *BmDataStorage.RequestStorage
 	var ss *BmDataStorage.PptinformationStorage
@@ -47,203 +51,6 @@ func (c PptinformationResource) NewPptinformationResource(args []BmDataStorage.B
 	return PptinformationResource{RequestStorage: cs,PptinformationStorage:ss,ChcpptStorage: ts,ChcppttemplateStorage: ps}
 }
 
-// FindAll Requests
-func (c PptinformationResource) FindAll(r api2go.Request) (api2go.Responder, error) {
-
-	results := c.PptinformationStorage.GetAll(r,-1,-1)
-	if len(results) <= 0{
-		return &Response{Res: results}, nil
-	}
-	if results[0].Url == ""{
-		c.GetUrl(results[0])
-		_= c.PptinformationStorage.Update(*results[0])
-		//time.Sleep(40*time.Second)
-	}
-	return &Response{Res: results}, nil
-}
-
-func (c PptinformationResource) GetUrl( result *BmModel.Pptinformation){
-	var iscreat int
-	var temp string
-	var url string
-	uuid, err := uuid.GenerateUUID()
-	if err != nil {
-		fmt.Println(err)
-		return 
-	}
-	url = c.GenPPT(uuid)
-	if url == ""{
-		painc("err")
-	}else{
-		url=""
-	}
-	time.Sleep(2000)
-	for i,data:=range result.Data{
-		var contentints []interface{}
-		iscreat=0	
-		dataMap,_:= data.(bson.M)
-		c.GetDatamap(dataMap,&temp,&contentints)
-		tmp,err := c.ChcppttemplateStorage.GetOne(temp)
-		if err != nil {
-			fmt.Println(err)
-			return 
-		}
-		Shapes:=tmp.Shapes
-		for j,contentint := range contentints{
-			var txt string
-			var name string
-			var css string
-			var shapeType string
-			var table string
-			var chart string
-			var pos []int
-			var cells []string
-			var formatstr string
-			if len(Shapes)>0{
-				Shapeint,_:=Shapes[j].(interface{})
-				Shape,_:= Shapeint.(bson.M)
-				c.GetShape(Shape,&pos,&shapeType,&formatstr,&cells,&name,&css)
-			}
-			
-			content,_:= contentint.(bson.M)
-			c.GetContent(content,formatstr,&txt,&table,&chart)
-			if txt=="end"{
-				url  = c.CreateSlider(uuid,"end","end",i)
-				result.Url = url
-				break
-			}
-			if iscreat==0{
-				url  = c.CreateSlider(uuid,tmp.Slider_Type,txt,i)
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)
-				iscreat=1
-			}
-			if temp!=""&&txt!=""{
-				url=c.PushText(uuid,txt,pos,i,shapeType)
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)
-			}else if temp!=""&&table!=""{
-				chcppts,_:=c.ChcpptStorage.GetOne(table)
-				for _,Cellint:=range chcppts.Cells{
-					var coordinate string
-					var value string
-					cell,_:=Cellint.(bson.M)
-
-					coordinateint:=cell["coordinate"]
-					if coordinateint!=nil{
-						coordinate=coordinateint.(string)
-					}
-					valueint:=cell["value"]
-					if valueint!=nil{
-						value=valueint.(string)
-					}
-					for t,celldata:=range cells{
-						reg := regexp.MustCompile("#c#[^#]+")
-						sitearr := reg.Find([]byte(celldata))
-						site:=string(sitearr)[3:]
-						if site==coordinate{
-							cells[t]=cells[t]+value
-						}
-					}
-				}		
-				name=table+temp
-				url  = c.ExcelPush(uuid ,name,cells)
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)
-				url  = c.Excel2PPT(uuid,name,pos,i)
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)			
-			}else if temp!=""&&chart!=""{
-				chcppts,_:=c.ChcpptStorage.GetOne(chart)
-				for _,Cellint:=range chcppts.Cells{
-					var coordinate string
-					var value string
-					cell,_:=Cellint.(bson.M)
-
-					coordinateint:=cell["coordinate"]
-					if coordinateint!=nil{
-						coordinate=coordinateint.(string)
-					}
-					valueint:=cell["value"]
-					if valueint!=nil{
-						value=valueint.(string)
-					}
-					for t,celldata:=range cells{
-						reg := regexp.MustCompile("#c#[^#]+")
-						sitearr := reg.Find([]byte(celldata))
-						site:=string(sitearr)[3:]
-						if site==coordinate{
-							cells[t]=cells[t]+value
-						}
-					}
-				}		
-				name=chart+temp	
-				url  = c.ExcelPush(uuid ,name,cells)
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)
-				url  = c.Excel2Chart(uuid,name,pos,i,shapeType,css)	
-				if url == ""{
-					painc("err")
-				}else{
-					url=""
-				}
-				time.Sleep(2000)	
-			}
-		}
-	}
-	url  = c.PushPPT(uuid)
-}
-
-func (c PptinformationResource) FindOne(ID string, r api2go.Request) (api2go.Responder, error) {
-	res, err := c.PptinformationStorage.GetOne(ID)
-	return &Response{Res: res}, err
-}
-
-func (c PptinformationResource) Create(obj interface{}, r api2go.Request) (api2go.Responder, error) {
-	choc, ok := obj.(BmModel.Pptinformation)
-	if !ok {
-		return &Response{}, api2go.NewHTTPError(errors.New("Invalid instance given"), "Invalid instance given", http.StatusBadRequest)
-	}
-
-	id := c.PptinformationStorage.Insert(choc)
-	choc.ID = id
-	return &Response{Res: choc, Code: http.StatusCreated}, nil
-}
-
-func (c PptinformationResource) Delete(id string, r api2go.Request) (api2go.Responder, error) {
-	err := c.PptinformationStorage.Delete(id)
-	return &Response{Code: http.StatusOK}, err
-}
-
-func (c PptinformationResource) Update(obj interface{}, r api2go.Request) (api2go.Responder, error) {
-	choc, ok := obj.(BmModel.Pptinformation)
-	if !ok {
-		return &Response{}, api2go.NewHTTPError(errors.New("Invalid instance given"), "Invalid instance given", http.StatusBadRequest)
-	}
-
-	err := c.PptinformationStorage.Update(choc)
-	return &Response{Res: choc, Code: http.StatusNoContent}, err
-}
 
 func (c PptinformationResource) GenPPT(jobid string) string {
 	var arr BmModel.Request
@@ -264,15 +71,20 @@ func (c PptinformationResource) GenPPT(jobid string) string {
 	}
 	str:=string(strbyt)
 	fmt.Println(str)
+	os.Setenv("BM_KAFKA_CONF_HOME", "../resource/kafkaconfig.json")
 
 	bkc, err := bmkafka.GetConfigInstance()
 	if err != nil {
 		panic(err.Error())
 	}
 	topic := "test"
-	bkc.Produce(&topic, str)
+	encoder := NewKafkaAvroEncoder(schemaRepositoryUrl)
+	schema, err := avro.ParseSchema(rawMetricsSchema)
+	record := avro.NewGenericRecord(schema)
+	strcode, err := encoder.Encode(record)
+	bkc.Produce(&topic, strcode)
 
-	topics := []string{"test"}
+	topics := []string{"test1"}
 	bkc.SubscribeTopics(topics, c.subscribeFunc)
 
 	// request, err := http.NewRequest("POST", "http://192.168.100.195:9999/api/ppt", filePtr)
@@ -286,6 +98,8 @@ func (c PptinformationResource) GenPPT(jobid string) string {
 	return url
 }
 func (c PptinformationResource)subscribeFunc(a interface{}) {
+	decoder := NewKafkaAvroDecoder(schemaRepositoryUrl)
+	decoded, err := decoder.Decode(bytes)
 	url=a.(string)
 }
 func (c PptinformationResource) CreateSlider(jobid string ,sliderType string , title string,slider int) string {
@@ -566,4 +380,199 @@ func (c PptinformationResource) GetDatamap(dataMap bson.M,temp*string,contentint
 	*contentints,_=contentsint.([]interface{})
 }
 
+func (c PptinformationResource) GetUrl( result *BmModel.Pptinformation){
+	var iscreat int
+	var temp string
+	var url string
+	uuid, err := uuid.GenerateUUID()
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	url = c.GenPPT(uuid)
+	if url == ""{
+		painc("err")
+	}else{
+		url=""
+	}
+	time.Sleep(2000)
+	for i,data:=range result.Data{
+		var contentints []interface{}
+		iscreat=0	
+		dataMap,_:= data.(bson.M)
+		c.GetDatamap(dataMap,&temp,&contentints)
+		tmp,err := c.ChcppttemplateStorage.GetOne(temp)
+		if err != nil {
+			fmt.Println(err)
+			return 
+		}
+		Shapes:=tmp.Shapes
+		for j,contentint := range contentints{
+			var txt string
+			var name string
+			var css string
+			var shapeType string
+			var table string
+			var chart string
+			var pos []int
+			var cells []string
+			var formatstr string
+			if len(Shapes)>0{
+				Shapeint,_:=Shapes[j].(interface{})
+				Shape,_:= Shapeint.(bson.M)
+				c.GetShape(Shape,&pos,&shapeType,&formatstr,&cells,&name,&css)
+			}
+			
+			content,_:= contentint.(bson.M)
+			c.GetContent(content,formatstr,&txt,&table,&chart)
+			if txt=="end"{
+				url  = c.CreateSlider(uuid,"end","end",i)
+				result.Url = url
+				break
+			}
+			if iscreat==0{
+				url  = c.CreateSlider(uuid,tmp.Slider_Type,txt,i)
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)
+				iscreat=1
+			}
+			if temp!=""&&txt!=""{
+				url=c.PushText(uuid,txt,pos,i,shapeType)
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)
+			}else if temp!=""&&table!=""{
+				chcppts,_:=c.ChcpptStorage.GetOne(table)
+				for _,Cellint:=range chcppts.Cells{
+					var coordinate string
+					var value string
+					cell,_:=Cellint.(bson.M)
 
+					coordinateint:=cell["coordinate"]
+					if coordinateint!=nil{
+						coordinate=coordinateint.(string)
+					}
+					valueint:=cell["value"]
+					if valueint!=nil{
+						value=valueint.(string)
+					}
+					for t,celldata:=range cells{
+						reg := regexp.MustCompile("#c#[^#]+")
+						sitearr := reg.Find([]byte(celldata))
+						site:=string(sitearr)[3:]
+						if site==coordinate{
+							cells[t]=cells[t]+value
+						}
+					}
+				}		
+				name=table+temp
+				url  = c.ExcelPush(uuid ,name,cells)
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)
+				url  = c.Excel2PPT(uuid,name,pos,i)
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)			
+			}else if temp!=""&&chart!=""{
+				chcppts,_:=c.ChcpptStorage.GetOne(chart)
+				for _,Cellint:=range chcppts.Cells{
+					var coordinate string
+					var value string
+					cell,_:=Cellint.(bson.M)
+
+					coordinateint:=cell["coordinate"]
+					if coordinateint!=nil{
+						coordinate=coordinateint.(string)
+					}
+					valueint:=cell["value"]
+					if valueint!=nil{
+						value=valueint.(string)
+					}
+					for t,celldata:=range cells{
+						reg := regexp.MustCompile("#c#[^#]+")
+						sitearr := reg.Find([]byte(celldata))
+						site:=string(sitearr)[3:]
+						if site==coordinate{
+							cells[t]=cells[t]+value
+						}
+					}
+				}		
+				name=chart+temp	
+				url  = c.ExcelPush(uuid ,name,cells)
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)
+				url  = c.Excel2Chart(uuid,name,pos,i,shapeType,css)	
+				if url == ""{
+					painc("err")
+				}else{
+					url=""
+				}
+				time.Sleep(2000)	
+			}
+		}
+	}
+	url  = c.PushPPT(uuid)
+}
+// FindAll Requests
+func (c PptinformationResource) FindAll(r api2go.Request) (api2go.Responder, error) {
+
+	results := c.PptinformationStorage.GetAll(r,-1,-1)
+	if len(results) <= 0{
+		return &Response{Res: results}, nil
+	}
+	if results[0].Url == ""{
+		c.GetUrl(results[0])
+		_= c.PptinformationStorage.Update(*results[0])
+		//time.Sleep(40*time.Second)
+	}
+	return &Response{Res: results}, nil
+}
+
+func (c PptinformationResource) FindOne(ID string, r api2go.Request) (api2go.Responder, error) {
+	res, err := c.PptinformationStorage.GetOne(ID)
+	return &Response{Res: res}, err
+}
+
+func (c PptinformationResource) Create(obj interface{}, r api2go.Request) (api2go.Responder, error) {
+	choc, ok := obj.(BmModel.Pptinformation)
+	if !ok {
+		return &Response{}, api2go.NewHTTPError(errors.New("Invalid instance given"), "Invalid instance given", http.StatusBadRequest)
+	}
+
+	id := c.PptinformationStorage.Insert(choc)
+	choc.ID = id
+	return &Response{Res: choc, Code: http.StatusCreated}, nil
+}
+
+func (c PptinformationResource) Delete(id string, r api2go.Request) (api2go.Responder, error) {
+	err := c.PptinformationStorage.Delete(id)
+	return &Response{Code: http.StatusOK}, err
+}
+
+func (c PptinformationResource) Update(obj interface{}, r api2go.Request) (api2go.Responder, error) {
+	choc, ok := obj.(BmModel.Pptinformation)
+	if !ok {
+		return &Response{}, api2go.NewHTTPError(errors.New("Invalid instance given"), "Invalid instance given", http.StatusBadRequest)
+	}
+
+	err := c.PptinformationStorage.Update(choc)
+	return &Response{Res: choc, Code: http.StatusNoContent}, err
+}
